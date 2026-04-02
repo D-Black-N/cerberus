a6971fc34719c301be7615b6ffe4cd4607e40ca4
D-Black-N <dan.nasibullin@gmail.com>
2026-04-02 19:44:49 +0300

    add settings to AR base class

--- lib/cerberus/infra/active_record/models/application_record.rb
+++ lib/cerberus/infra/active_record/models/application_record.rb
@@ -5,6 +5,9 @@ module Cerberus
     module ActiveRecord
       module Models
         class ApplicationRecord < ActiveRecord::Base
+          self.abstract_class = true
+          self.table_name_prefix = 'cerberus_'
+
           def self.namespace
             name.gsub(/\w+\z/, '')
           end

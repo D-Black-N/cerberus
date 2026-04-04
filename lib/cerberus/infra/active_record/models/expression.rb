94c4fb2985d283b186df681a2939924e9243669b
D-Black-N <dan.nasibullin@gmail.com>
2026-04-04 16:43:01 +0300

    belongs_to optional for expression

--- lib/cerberus/infra/active_record/models/expression.rb
+++ lib/cerberus/infra/active_record/models/expression.rb
@@ -5,8 +5,8 @@ module Cerberus
     module ActiveRecord
       module Models
         class Expression < ApplicationRecord
-          belongs_to :rule, class_name: "#{namespace}Rule"
-          belongs_to :parent, class_name: "#{namespace}Expression"
+          belongs_to :rule, class_name: "#{namespace}Rule", optional: true
+          belongs_to :parent, class_name: "#{namespace}Expression", optional: true
         end
       end
     end
